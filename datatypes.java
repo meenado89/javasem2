d732fe74f4404e9a9050531d0abc845223497408
meenado89 <meenakshiemail89@gmail.com>
2026-02-02 15:09:26 +0530

    printing diffrent dataypes

---
+++ datatypes.java
@@ -0,0 +1,25 @@
+public class datatypes {
+    public static void main (String []args)
+    {
+        // DECLAIRING VARIABLES OF PREMITIVE DATA TYPES
+        int age = 20;
+        double salary = 25000.75;
+        char grades = 'A';
+        boolean isStudent = true;
+        float height = 5.8f;
+        byte level = 3;
+        short year = 2024;
+        long population = 138000000l;
+
+        // DISPLAYING VALUE
+        System.out.println("age: "+age);
+        System.out.println("salary: " +salary);
+        System.out.println("grades: "+grades);
+        System.out.println("isStudent: " +isStudent);
+        System.out.println("height: "+height);
+        System.out.println("level: "+level);
+        System.out.println("year: "+year);
+        System.out.println("population: "+population);
+    }
+
+}
