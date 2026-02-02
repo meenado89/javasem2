4c049ac3b846279fd8b67870364ae1e63c23d21c
meenado89 <meenakshiemail89@gmail.com>
2026-02-02 16:00:13 +0530

    loop

--- loop.java
+++ loop.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 public class loop {
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+         System.out.println("\nNumbers from 1 to 10 using for loop:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
 
 }
