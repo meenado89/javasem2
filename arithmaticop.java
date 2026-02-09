4be69a676cd81d1df7a2e502b789e82185f37f5b
meenado89 <meenakshiemail89@gmail.com>
2026-02-09 16:10:29 +0530

    syntax fix

--- arithmaticop.java
+++ arithmaticop.java
@@ -8,7 +8,7 @@ class arithmaticop{
         int b = sc.nextInt();
         System.out.println("Addition:"+(a+b));
         System.out.println("Subtraction:"+(a-b));
-        System.out.println("Multiplicatio:"+(a*b));
+        System.out.println("Multiplication:"+(a*b));
         System.out.println("Division"+(a/b));
     }
 }
