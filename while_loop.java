3810ec7384b93f4b9fe9806a3c774ae4db6696ef
meenado89 <meenakshiemail89@gmail.com>
2026-02-10 13:20:12 +0530

    while loop

---
+++ while_loop.java
@@ -0,0 +1,13 @@
+class while_loop{
+  public static void main(String[] args)
+    {
+        int i = 0;
+        while (i <= 10) {
+
+             i++;
+            System.out.print(i + " ");
+           
+        }
+    }
+
+}
